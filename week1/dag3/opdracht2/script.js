63511ca819a4da8dd18315a0f4269e6b360a0b60
lessanonymous <63210198+lessanonymous@users.noreply.github.com>
2020-05-06 11:22:31 +0200

    W1D3-2 afgerond

---
+++ week1/dag3/opdracht2/script.js
@@ -0,0 +1,33 @@
+// makeCheeseSandwich
+// add slice of bread.
+// add slice of cheese.
+// add slice of bread.
+
+const makeCheeseSandwich = () => {
+    console.log('add slice of bread.');
+    console.log('add slice of cheese.');
+    console.log('add slice of bread.');
+};
+
+makeCheeseSandwich();
+
+// function declaration(arrow notation)
+const makeSandwich = topping => console.log(`There you go, a sandwich with ${topping}`);
+
+// function call
+makeSandwich('Cheese');
+makeSandwich('Chicken');
+
+const calculateDiscountedPrice = (totalAmount, discount) => Math.round(totalAmount - discount);
+
+console.log(calculateDiscountedPrice(120, 23.45));
+
+const newCalculateDiscountedPrice = (totalAmount, discount) => {
+    if (totalAmount > 25) {
+        return Math.round(totalAmount - discount);
+    }
+    return totalAmount;
+};
+
+console.log(newCalculateDiscountedPrice(120, 23.45));
+console.log(newCalculateDiscountedPrice(20, 23.45));
