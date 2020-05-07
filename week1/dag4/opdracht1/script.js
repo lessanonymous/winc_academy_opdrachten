c4b008d2e529b66a44623263f2467c4740f6df55
lessanonymous <63210198+lessanonymous@users.noreply.github.com>
2020-05-07 11:22:13 +0200

    W1D4-1 afgerond

---
+++ week1/dag4/opdracht1/script.js
@@ -0,0 +1,3 @@
+/* document.body.innerHTML = 'Ik ben de nieuwe inhoud van body';
+const firstSection = document.getElementById('first-section');
+const sections = document.getElementsByClassName('paragraph'); */
