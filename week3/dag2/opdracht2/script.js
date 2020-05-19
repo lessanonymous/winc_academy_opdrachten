83defc342acf663eede1711cf694941faedbd8dc
lessanonymous <63210198+lessanonymous@users.noreply.github.com>
2020-05-19 11:54:35 +0200

    W3D2-1 en W3D2-2 afgerond

---
+++ week3/dag2/opdracht2/script.js
@@ -0,0 +1,10 @@
+const huiswerkMaken = (vak, callback) => {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+    setTimeout(callback, 1000);
+};
+
+const klaarMetHuiswerk = () => {
+    console.log(`Kijk Paps/Mams, ik ben klaar met mijn huiswerk!`);
+};
+
+huiswerkMaken('Wiskunde', klaarMetHuiswerk);
